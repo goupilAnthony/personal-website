c68330558f3f66d547b01d427c46a0bd0f0a6730
goupilAnthony <anthony.93460@gmail.com>
2020-11-23 00:36:50 +0100

    createPage 404 conf

---
+++ gatsby-node.js
@@ -0,0 +1,16 @@
+const standardBasePath = `/`
+
+exports.createPages = async ({ actions }, themeOptions) => {
+  const { createPage } = actions
+
+  const basePath = themeOptions.basePath || standardBasePath
+
+  createPage({
+    path: basePath,
+    component: require.resolve(`./src/@lekoarts/gatsby-theme-cara/templates/cara.tsx`),
+  })
+  createPage({
+    path: "404",
+    component: require.resolve(`./src/@lekoarts/gatsby-theme-cara/templates/404.js`),
+  })
+}
