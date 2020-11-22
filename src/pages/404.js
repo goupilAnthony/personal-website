fed29208b1ebea033133a46c346dd3343dd6f263
goupilAnthony <anthony.93460@gmail.com>
2020-11-22 23:05:30 +0100

    add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import hero from '../@lekoarts/gatsby-theme-cara/components/hero'
+
+const Unknow = () => {
+    return (
+        <Hero />
+    )
+}
+
+export default Unknow
