ff889d0232baf5f183704a4b54b7dd784d59f830
goupilAnthony <anthony.93460@gmail.com>
2020-11-24 21:29:11 +0100

    style 404 text

--- src/@lekoarts/gatsby-theme-cara/components/Custom404.jsx
+++ src/@lekoarts/gatsby-theme-cara/components/Custom404.jsx
@@ -3,13 +3,15 @@ import styled from 'styled-components'
 import { Typography } from '@material-ui/core'
 
 const Background404 = styled(Typography)`
-
+    color: #d24747b3 !important;
+    font-size: 6rem !important;
 `
 
 const Custom404 = () => {
     return (
         <>
             <Background404 variant="body2" align="center" >404</Background404>
+            <Typography variant="body2" align="center">Je n'ai pas trouvé la page que tu as demandé !</Typography>
         </>
     )
 }
