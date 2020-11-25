ba0448bca84deeca8bc7204a01df249cb00ce04f
Zanea <anthony.93460@gmail.com>
2020-11-25 22:28:14 +0100

    remove unused

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,16 +1,10 @@
 import React from 'react'
 import Layout from "../@lekoarts/gatsby-theme-cara/components/layout"
 import { Parallax } from "react-spring/renderprops-addons.cjs"
-import Hero from "../@lekoarts/gatsby-theme-cara/components/hero"
-import styled from 'styled-components'
 import CustomHero404 from '../@lekoarts/gatsby-theme-cara/components/CustomHero404'
 import CustomFooterLayout from '../@lekoarts/gatsby-theme-cara/components/CustomFooterLayout'
 
-const UnknowPage = styled.div`
-    width: 100%;
-    height: 100%;
-    background-color: #141821;
-`
+
 
 const Unknow = () => {
     return (
