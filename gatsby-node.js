938181318ecba4a7873eb8063cf7f62c3400784e
Zanea <anthony.93460@gmail.com>
2020-11-25 22:26:46 +0100

    remove 404 path declaration

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,8 +9,10 @@ exports.createPages = async ({ actions }, themeOptions) => {
     path: basePath,
     component: require.resolve(`./src/@lekoarts/gatsby-theme-cara/templates/cara.tsx`),
   })
-  createPage({
-    path: "404",
-    component: require.resolve(`./src/pages/404.js`),
-  })
+
 }
+
+//createPage({
+//  path: "404",
+//  component: require.resolve(`./src/pages/404.js`),
+//})
